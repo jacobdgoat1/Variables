02437902359ac5a476510fbeac874ec26002d79a
Jacob Thomas <thomajac002@warren.k12.in.us>
2024-08-15 09:20:18 -0400

    intial commmit

---
+++ main.py
@@ -0,0 +1,8 @@
+num_apples=20
+num_oranges=15
+print("Number of apples", num_apples)
+print("Number of oranges",num_oranges)
+num_apples=20
+num_oranges=0
+print("Number of apples",num_apples)
+print("Number of oranges",num_oranges)
